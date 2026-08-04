9bf2d32abeed0ed5c16ff1d4f09f3a6d7b29512b
i_am_ZhVi <unius6ls66@gmail.com>
2026-08-04 12:38:08 +0300

    file table migration

--- migrations/env.py
+++ migrations/env.py
@@ -1,5 +1,7 @@
 import asyncio
 from logging.config import fileConfig
+import os
+import sys
 
 from sqlalchemy import pool
 from sqlalchemy.engine import Connection
@@ -7,9 +9,16 @@ from sqlalchemy.ext.asyncio import async_engine_from_config
 
 from alembic import context
 
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+
+BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+sys.path.insert(0, os.path.join(BASE_DIR, "app"))
+
+from app.core.database import db_config
+from app.infrastructure.db_models import Base
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
@@ -20,12 +29,14 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
 # my_important_option = config.get_main_option("my_important_option")
 # ... etc.
+
+config.set_main_option("sqlalchemy.url", str(db_config.database_url))
 
 
 def run_migrations_offline() -> None:
