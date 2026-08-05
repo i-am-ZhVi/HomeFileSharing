c2fea8c6d57a33a559727cffaae9f0c32fa01755
i_am_ZhVi <unius6ls66@gmail.com>
2026-08-05 22:30:13 +0300

    update file extension to non nullable

--- app/infrastructure/db_models/file_table.py
+++ app/infrastructure/db_models/file_table.py
@@ -9,7 +9,7 @@ from infrastructure.db_models.base_table import Base
 class File(Base):
     id: Mapped[int] = mapped_column(nullable=False, primary_key=True, index=True)
     name: Mapped[str] = mapped_column(nullable=False)
-    extension_id: Mapped[int] = mapped_column(ForeignKey("extension.id"), nullable=True)
+    extension_id: Mapped[int] = mapped_column(ForeignKey("extension.id"), nullable=False)
     mime_type: Mapped[str] = mapped_column(nullable=True)
     category_id: Mapped[int] = mapped_column(ForeignKey("category.id"), nullable=False)
     password_hash: Mapped[str] = mapped_column(nullable=True)
