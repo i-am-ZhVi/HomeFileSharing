9d0c045abe53ca036d098af0ca05ab9e97a68098
i_am_ZhVi <unius6ls66@gmail.com>
2026-08-05 20:01:49 +0300

    fix: modules import error

--- app/infrastructure/db_models/extension_table.py
+++ app/infrastructure/db_models/extension_table.py
@@ -1,5 +1,5 @@
 from sqlalchemy.orm import Mapped, mapped_column, relationship
-from app.infrastructure.db_models.base_table import Base
+from infrastructure.db_models.base_table import Base
 
 
 class Extension(Base):
