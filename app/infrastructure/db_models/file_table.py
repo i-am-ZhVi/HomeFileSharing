b71d0783b36ea3b019116522f83bf158bf74659e
i_am_ZhVi <unius6ls66@gmail.com>
2026-08-05 10:28:21 +0300

    file table extension

--- app/infrastructure/db_models/file_table.py
+++ app/infrastructure/db_models/file_table.py
@@ -1,8 +1,16 @@
 
+from datetime import datetime, tzinfo
 from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.sql import text
 from infrastructure.db_models.base_table import Base
 
 
 class File(Base):
-    id: Mapped[int] = mapped_column(primary_key=True)
+    id: Mapped[int] = mapped_column(nullable=False, primary_key=True)
     name: Mapped[str] = mapped_column(nullable=False)
+    extension: Mapped[str] = mapped_column(nullable=True)
+    mime_type: Mapped[str] = mapped_column(nullable=True)
+    category: Mapped[str] = mapped_column(nullable=False, default="other", server_default="other")
+    uploader_name: Mapped[str] = mapped_column(nullable=False, default="anonimous", server_default="anonimous")
+    password_hash: Mapped[str] = mapped_column(nullable=True)
+    upload_at: Mapped[datetime] = mapped_column(nullable=False, default=datetime.now(), server_default=text("TIMEZONE('utc', now())"))
