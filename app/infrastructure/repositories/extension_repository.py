c8ccf0272294a3dd0b13a8d2e5fa4ca8ca46bc8a
i_am_ZhVi <unius6ls66@gmail.com>
2026-08-06 22:24:36 +0300

    feat: The extension repository has been brought to solid.

--- app/infrastructure/repositories/extension_repository.py
+++ app/infrastructure/repositories/extension_repository.py
@@ -1,4 +1,4 @@
-from sqlalchemy import select, true
+from sqlalchemy import desc, select, true
 from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import selectinload
@@ -11,30 +11,40 @@ class SQLAlchemyExtensionRepository(ExtensionRepository):
     def __init__(self, session: AsyncSession):
         self.session = session
 
-    async def get(self, extension_id: int | None, sub_name: str | None) -> Extension | list[Extension] | None:
+    async def get_by_id(self, id: int) -> Extension | None:
         logger.debug(
-            "Extension repository: get extensions. Params: "
-            f"extension_id={extension_id}, sub_name={sub_name}."
+            "Extension repository: get extension by id. Params: "
+            f"id={id}."
         )
         try:
-            if extension_id:
-                response = await self.session.execute(select(Extension).where(Extension.id == extension_id))
-                result = response.scalar_one_or_none()
+            response = await self.session.execute(select(Extension).where(Extension.id == id).options(selectinload(Extension.files)))
+            result = response.scalar_one_or_none()
 
-                if result:
-                    logger.info(f"Extension repository: found extension_id={result.id}.")
-                else:
-                    logger.warning(f"Extension repository: extension_id={extension_id} not found.")
+            if result:
+                logger.info(f"Extension repository: found id={result.id}.")
+            else:
+                logger.warning(f"Extension repository: id={id} not found.")
 
-                return result
+            return result
+        except SQLAlchemyError:
+            logger.exception("Extension repository: database error coccurred during get operation workflow.")
+            raise
 
-            response = await self.session.execute(select(Extension).options(selectinload(Extension.files))
-                .where(Extension.name.contains(sub_name) if sub_name else true()))
+    async def get_list(self, sub_name: str | None) -> list[Extension]:
+        logger.debug(
+            "Extension repository: get list extensions. Params: "
+            f"sub_name={sub_name}."
+        )
+        try:
+            query = select(Extension).options(selectinload(Extension.files)).order_by(desc(Extension.created_at), desc(Extension.id))
 
+            if sub_name:
+                query = query.where(Extension.name.contains(sub_name))
+
+            response = await self.session.execute(query)
             result = list(response.scalars().all())
 
             logger.info(f"Extension repository: found {len(result)} extensions matching filters.")
-
             return result
 
         except SQLAlchemyError:
@@ -66,7 +76,7 @@ class SQLAlchemyExtensionRepository(ExtensionRepository):
             f"extension_id={extension_id}, extension_name={extension_name}."
         )
         try:
-            response = await self.session.execute(select(Extension).where(Extension.id == extension_id))
+            response = await self.session.execute(select(Extension).where(Extension.id == extension_id).options(selectinload(Extension.files)))
 
             extension = response.scalar_one_or_none()
             if not extension:
