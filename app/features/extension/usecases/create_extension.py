37df3c98dbbffe600793d27e9c4d94dfae1e505a
i_am_ZhVi <unius6ls66@gmail.com>
2026-08-06 20:13:37 +0300

    feat: create usecase for extension create

---
+++ app/features/extension/usecases/create_extension.py
@@ -0,0 +1,10 @@
+from app.features.extension.repositories.interface import ExtensionRepository
+from app.infrastructure.db_models.extension_table import Extension
+
+
+class CreateExtensionUseCase:
+    def __init__(self, repo: ExtensionRepository):
+        self.repo = repo
+
+    async def execute(self, name: str) -> Extension | None:
+        return await self.repo.create(extension_name=name)
