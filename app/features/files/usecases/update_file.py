a406d382fa2943daab3ae685d79b7794c813e42d
i_am_ZhVi <unius6ls66@gmail.com>
2026-08-06 20:29:57 +0300

    feat: create usecase for file update

---
+++ app/features/files/usecases/update_file.py
@@ -0,0 +1,11 @@
+
+from app.features.files.repositories.interface import FileRepository
+from app.infrastructure.db_models.file_table import File
+
+
+class UpdateFileUseCase:
+    def __init__(self, repo: FileRepository):
+        self.repo = repo
+
+    async def execute(self, file_id: int, name: str | None, extension_id: int | None, mime_type_id: int | None, category_id: int | None, password_hash: str | None) -> File | None:
+        return await self.repo.update(file_id=file_id, name=name, extension_id=extension_id, mime_type_id=mime_type_id, category_id=category_id, password_hash=password_hash)
