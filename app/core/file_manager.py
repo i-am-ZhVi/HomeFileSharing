19c1551bf7233d117671b328cf40aec986f60a41
i_am_ZhVi <unius6ls66@gmail.com>
2026-08-08 12:02:09 +0300

    fix: file manager naming saved files

--- app/core/file_manager.py
+++ app/core/file_manager.py
@@ -3,15 +3,18 @@ from uuid import UUID
 import aiofiles
 from fastapi import UploadFile
 
+from core.config import settings
+
 
 class FileManager:
     async def save_file_to_directory(self, file: UploadFile, version_uuid: UUID, upload_dir: str):
         file_name = ""
-        if file.filename:
+        if file.filename and not settings.FILE_CRYPT:
             name = Path(file.filename).name
             extension = Path(file.filename).suffix
             file_name += name + str(version_uuid) + extension
         else: file_name = str(version_uuid)
+
 
         path = Path(upload_dir)
         path.mkdir(parents=True, exist_ok=True)
