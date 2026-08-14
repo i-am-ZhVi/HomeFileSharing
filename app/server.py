f881bc1af1e7b088246f75177b89458efe3b28f8
i_am_ZhVi <unius6ls66@gmail.com>
2026-08-14 17:56:09 +0300

    feat: cors middleware

--- app/server.py
+++ app/server.py
@@ -1,5 +1,7 @@
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from core.config import settings
 from features.routes import router as api_router
 
 
@@ -34,6 +36,14 @@ async def lifespan(app: FastAPI):
 app = FastAPI(title="Home file sharing", docs_url="/api/docs", redoc_url="/api/redoc", openapi_url="/api/openapi.json", swagger_ui_oauth2_redirect_url="/api/docs/oauth2-redirect", lifespan=lifespan)
 
 app.include_router(api_router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=settings.CORS_ALLOWED_ORIGINS,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/api")
 def root():
